e63b0c87e6ecae248c1fd9167578f0b3ceff852b
Aarushi Tiwari <aarushitiwari04@gmail.com>
2025-06-22 15:43:22 +0530

    Backend Updated

--- src/main/java/com/example/courses_api/CoursesApiApplication.java
+++ src/main/java/com/example/courses_api/CoursesApiApplication.java
@@ -2,8 +2,12 @@ package com.example.courses_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.courses_api.repository")
+@EntityScan(basePackages = "com.example.courses_api.entity")
 public class CoursesApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoursesApiApplication.class, args);
